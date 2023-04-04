581ef8bd403a00779f80298a2b35a0bb246d6668
Genivania <109962733+Genivania@users.noreply.github.com>
2023-04-04 11:03:41 -0300

    okkk

--- app/src/main/java/br/senai/sp/jandira/triproom/repository/UserRepository.kt
+++ app/src/main/java/br/senai/sp/jandira/triproom/repository/UserRepository.kt
@@ -1,7 +1,15 @@
 package br.senai.sp.jandira.triproom.repository
 
-class UserRepository {
+import android.content.Context
+import br.senai.sp.jandira.triproom.dao.TripDb
+import br.senai.sp.jandira.triproom.model.User
 
+class UserRepository(context: Context) {
 
+    //Variável que representa nosso banco de dados
+    private val  db = TripDb.getDataBase(context)
 
+    fun save(user: User): Long{
+        return db.userDao().save(user)
+    }
 }
