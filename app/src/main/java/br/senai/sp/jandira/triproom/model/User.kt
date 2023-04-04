a9200f7dec783efe2daec282e79c89f9e9e0c5f5
Genivania <109962733+Genivania@users.noreply.github.com>
2023-04-04 10:54:32 -0300

    criando

--- app/src/main/java/br/senai/sp/jandira/triproom/model/User.kt
+++ app/src/main/java/br/senai/sp/jandira/triproom/model/User.kt
@@ -9,9 +9,9 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "tbl_user")
 data class User(
     @PrimaryKey(autoGenerate = true) var id: Long = 0,
-    @ColumnInfo(name = "user_name") var userName : String = "",
+    @ColumnInfo(name = "user_name") var userName: String = "",
     var email: String = "",
-    var password : String = "",
+    var password: String = "",
     var phone: String = "",
     @ColumnInfo(name = "is_over_18") var isOver18: Boolean = false
 
---
+++ app/src/main/java/br/senai/sp/jandira/triproom/repository/UserRepository.kt
@@ -0,0 +1,5 @@
+package br.senai.sp.jandira.triproom.repository
+
+class UserRepository {
+
+}
